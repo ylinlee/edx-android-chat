6eb630a6a156fa4fc45f0c2b18915fe636668073
Yichuan <ylin@ext.vectorsf.com>
2016-07-18 12:00:12 +0200

    Chat ContactList Interactors

--- app/src/main/java/edu/galileo/android/androidchat/contactlist/ContactListInteractorImpl.java
+++ app/src/main/java/edu/galileo/android/androidchat/contactlist/ContactListInteractorImpl.java
@@ -4,23 +4,28 @@ package edu.galileo.android.androidchat.contactlist;
  * Created by Usuario on 18/07/2016.
  */
 public class ContactListInteractorImpl implements ContactListInteractor {
+    ContactListRepository repository;
+
+    public ContactListInteractorImpl() {
+        repository = new ContactListRepositoryImpl();
+    }
     @Override
     public void subscribe() {
-
+        repository.subscribeToContactListEvents();
     }
 
     @Override
     public void unsubscribe() {
-
+        repository.unsubscribeToContactListEvents();
     }
 
     @Override
     public void destroyListener() {
-
+        repository.destroyListener();
     }
 
     @Override
     public void removeContactList(String email) {
-
+        repository.removeContact(email);
     }
 }
--- app/src/main/java/edu/galileo/android/androidchat/contactlist/ContactListSessionInteractorImpl.java
+++ app/src/main/java/edu/galileo/android/androidchat/contactlist/ContactListSessionInteractorImpl.java
@@ -4,18 +4,24 @@ package edu.galileo.android.androidchat.contactlist;
  * Created by Usuario on 18/07/2016.
  */
 public class ContactListSessionInteractorImpl implements ContactListSessionInteractor {
+    ContactListRepository repository;
+
+    public ContactListSessionInteractorImpl() {
+        repository = new ContactListRepositoryImpl();
+    }
+
     @Override
     public void signOff() {
-
+    repository.signOff();
     }
 
     @Override
     public String getCurrentUserEmail() {
-        return null;
+        return repository.getCurrentUserEmail();
     }
 
     @Override
     public void changeConnectionStatus(boolean online) {
-
+        repository.changeConnectionStatus(online);
     }
 }
